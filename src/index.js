8f204f2ab477d4d58c27f7bbad2c71424393e30d
Joseph Morse <tamed.lionheart@gmail.com>
2019-04-20 08:57:19 -0400

    fix: fixed some issues
    I fixed some issues you where having I think and left some comments
    about some of the changes I made that I hope that will help.

--- src/index.js
+++ src/index.js
@@ -1,32 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+
 import { store } from './store';
 import './index.css';
-import { App } from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { connect } from 'react-redux';
-import { addMessage } from './action-creators/simpleAction';
-
-const mapStateToProps = (state) => {
- return {messages: state}
-};
-
-const mapDispatchToProps = (dispatch) => {
- return {
-   submitNewMessage: (message) => {
-     dispatch(addMessage(message))
-   }
- }
-};
-
-const Container = connect(mapStateToProps, mapDispatchToProps)(App);
-
 
 ReactDOM.render(
      <Provider store={store}>
-       <Container />
-     </Provider>, 
+       <App />
+     </Provider>,
      document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
